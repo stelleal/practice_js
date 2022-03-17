89c7e6a4211b637d40c26a6dcd741f8d1f42cbbe
stelleal <stellalleal@gmail.com>
2022-03-17 16:44:52 -0300

    att aulas

---
+++ mais-js/rest-e-spread/index.js
@@ -0,0 +1,21 @@
+// Reescreva a função utilizando
+// valores iniciais de parâmetros com ES6
+function createButton(background = 'blue', color = 'red') {
+  const buttonElement = document.createElement('button');
+  buttonElement.innerText = 'Botão'
+  buttonElement.style.background = background;
+  buttonElement.style.color = color;
+  return buttonElement;
+} 
+
+const redButton = createButton('tomato');
+const body = document.querySelector('body');
+body.appendChild(redButton);
+
+// Utilize o método push para inserir as frutas ao final de comidas.
+const frutas = ['Banana', 'Uva', 'Morango'];
+const comidas = ['Pizza', 'Batata'];
+
+comidas.push(...frutas)
+
+console.log(comidas)
